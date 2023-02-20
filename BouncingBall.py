c10a0718a88127b87478beab0923550e5e4bbb6e
Roland Labana <rolandlabana@gmail.com>
2023-02-20 09:21:37 -0800

    Added friction so ball stops rolling

--- BouncingBall.py
+++ BouncingBall.py
@@ -12,6 +12,7 @@ TOP_EDGE = 400
 
 GRAVITY = .1
 DAMPING = .8
+FRICTION = .02
 
 #This function will update the location of the ball
 def moveBall():
@@ -19,25 +20,40 @@ def moveBall():
 
     #update the postiion of the ball
     x = ball.xcor()
-    ball.setx(x + xVel)
+    if xVel != 0: # we have not stopped rolling due to friction
+        ball.setx(x + xVel)
         
     y = ball.ycor()
-    yVel = yVel - GRAVITY   #only y is impacted by gravity
-    ball.sety(y + yVel)
+    if yVel!=0: #if it's 0 we are not bouncing, we are rolling
+        yVel = yVel - GRAVITY   #only y is impacted by gravity
+        ball.sety(y + yVel)
+    else:   # friction comes into play while rolling which impacts xVel
+        if (xVel>0):  xVel = xVel-FRICTION
+        if (xVel<0):  xVel = xVel+FRICTION
+        if abs(xVel)>.005:  #we are still rolling
+            # print(xVel) - debug
+            pass
+        else:  # we are done - ball stopped bouncing and then stopped rolling
+            exit()
 
     #Check for collisons and reverse the direction if so
     if (x >= RIGHT_EDGE):
         xVel *= -1
-        ball.setx(x + xVel-5)
+        if xVel!=0:
+            ball.setx(x + xVel-5)
 
     if (x <= LEFT_EDGE):
         xVel *= -1
-        ball.setx(x + xVel+5)
+        if xVel!=0:
+            ball.setx(x + xVel+5)
    
-    if (y <= BOTTOM_EDGE):
+    if (y <= BOTTOM_EDGE+5):
         yVel *= -1
         yVel = yVel * DAMPING #damping effect
-        ball.sety(y + yVel+5)
+        if yVel>2:
+            ball.sety(y + yVel+5)
+        else:
+            yVel=0
 
     if (y >= TOP_EDGE):
         yVel *= -1
@@ -58,12 +74,11 @@ ball.shape("circle")
 ball.color("blue")
 ball.position()
 ball.speed(0)
-ball.sety(0)
-ball.setheading(20)
+ball.setheading(40)
 
 #Define initial position and speed of the ball
-ballX = 0
-ballY = 0
+ball.setx(100)
+ball.sety(200)
 xVel = 5
 yVel = 3
 
